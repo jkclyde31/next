9da7d1fa3ad4e84b5c4e900faf02a0dfc5c56f96
Clyde <randa.technodream@gmail.com>
2024-08-28 13:55:38 +0800

    first commit

---
+++ app/(pages)/home/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AboutSection from '@/app/component/sections/About-section'
+import FAQKitchenSection from '@/app/component/sections/Faqs-section'
+
+const page = () => {
+  return (
+    <div className=''>
+        <AboutSection/>
+        <FAQKitchenSection/>
+    </div>
+  )
+}
+
+export default page
