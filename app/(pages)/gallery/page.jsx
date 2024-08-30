30467b4b506632fcd6c556fd036da339b3d1b52a
Clyde <randa.technodream@gmail.com>
2024-08-30 14:30:03 +0800

    try again

--- app/(pages)/gallery/page.jsx
+++ app/(pages)/gallery/page.jsx
@@ -5,13 +5,13 @@ import { RenderBuilderContent } from "@/components/builder";
 builder.init('2fa337738072419589aaa019bd9defbc');
 
 export default async function Page(props) {
-  const model = "page";
+  const model = "gallery-model";
   const content = await builder
     // Get the page content from Builder with the specified options
-    .get("page", {
+    .get(model, {
       userAttributes: {
         // Use the page path specified in the URL to fetch the content
-        urlPath: "/about" + (props?.params?.page?.join("/") || ""),
+        urlPath: "/gallery" + (props?.params?.page?.join("/") || ""),
       },
       // Set prerender to false to return JSON instead of HTML
       prerender: false,
